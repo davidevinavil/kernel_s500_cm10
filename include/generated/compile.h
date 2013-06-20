692d3b497377189d26d196a05dfe4fe4c8372658
davidevinavil <davidevinavil@gmail.com>
2013-06-20 23:30:08 +0200

    Clean up

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 24 */
+/* This file is auto generated, version 48 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#24 SMP PREEMPT Fri May 24 13:23:27 CEST 2013"
+#define UTS_VERSION "#48 SMP PREEMPT Thu Jun 20 23:13:28 CEST 2013"
 #define LINUX_COMPILE_BY "Davide"
 #define LINUX_COMPILE_HOST "D M"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
